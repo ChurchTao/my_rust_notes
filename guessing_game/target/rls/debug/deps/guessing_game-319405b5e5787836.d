7a771b77ab4bd855358213c2609eca4f8b33b80b
taojiacheng <taojiacheng@dxy.cn>
2022-01-10 10:48:03 +0800

    gitignore

--- guessing_game/target/rls/debug/deps/guessing_game-319405b5e5787836.d
+++ guessing_game/target/rls/debug/deps/guessing_game-319405b5e5787836.d
@@ -1,5 +1,5 @@
-/Users/churcht/vsProjects/my_rust_learning/guessing_game/target/rls/debug/deps/guessing_game-319405b5e5787836.rmeta: guessing_game/src/main.rs
+/Users/churcht/vsProjects/my_rust_notes/guessing_game/target/rls/debug/deps/guessing_game-319405b5e5787836.rmeta: guessing_game/src/main.rs
 
-/Users/churcht/vsProjects/my_rust_learning/guessing_game/target/rls/debug/deps/guessing_game-319405b5e5787836.d: guessing_game/src/main.rs
+/Users/churcht/vsProjects/my_rust_notes/guessing_game/target/rls/debug/deps/guessing_game-319405b5e5787836.d: guessing_game/src/main.rs
 
 guessing_game/src/main.rs:
