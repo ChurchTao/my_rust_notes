d35f6fe3d0bcbe864d8350815f507b246dc2f39d
taojiacheng <taojiacheng@dxy.cn>
2022-01-12 18:55:51 +0800

    2022-01-12

--- t_test/src/main.rs
+++ t_test/src/main.rs
@@ -1,17 +1,30 @@
 fn main() {
     println!("Hello, world!");
-    let list = vec![10,21,7,-1,33,43];
-    println!("Hello, world! max = {}",largest(&list));
-    let list = vec!['c','a','b','1','l','z'];
-    println!("Hello, world! max = {}",largest(&list));
+    let list = vec![10, 21, 7, -1, 33, 43];
+    println!("Hello, world! max = {}", largest(&list));
+    println!("Hello, world! max = {}", largest2(&list));
+    let list = vec!['c', 'a', 'b', '1', 'l', 'z'];
+    println!("Hello, world! max = {}", largest(&list));
+    println!("Hello, world! max = {}", largest2(&list));
 }
 
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut max_item = &list[0];
-    for item in list.iter() {
-        if max_item < item {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut max_item = list[0];
+    for &item in list.iter() {
+        if item > max_item {
             max_item = item;
         }
     }
-    return max_item;
+    max_item
+}
+
+fn largest2<T>(list: &[T]) -> T where
+    T: PartialOrd + Copy {
+    let mut max_item = list[0];
+    for &item in list.iter() {
+        if item > max_item {
+            max_item = item;
+        }
+    }
+    max_item
 }
